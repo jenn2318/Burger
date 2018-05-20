411bf96818db8bc1b3007b94263af531ac02861a
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-05-20 19:16:25 -0400

    cleaned up code adjusted routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,52 +5,47 @@ let burger = require('../models/burger.js');
 
 //Let's make some routes
 
-// router.get("/", function (req, res) {
-//
-//    res.render('index');
-// });
+router.get("/", function (req, res) {
+
+    // res.render('index');
+    burger.selectAll(function(data) {
+        let hbsObject = {burger: data};
+        res.render('index', hbsObject);
+    });
+});
 
 // Send all my burgers to the DOM
 
-router.get("/", function(req, res) {
+router.get("/index", function(req, res) {
     burger.selectAll(function(data) {
         let hbsObject = {burger: data};
-        //console.log("burger_controller.js get: " + hbsObject);
         res.render('index', hbsObject);
     });
 });
 
 
 // We will make a new burger
-router.post("/api/burgers", function(req, res) {
-    console.log(req.body.burger_name);
-       burger.insertOne(req.body.burger_name, function(result){
-       // console.log("Post :" + result);
+router.post("/api/burgers/", function(req, res) {
+    burger.insertOne(req.body.burger_name, function(){
         res.redirect('/');
-
-        //res.redirect('/index');
     });
 });
 
 
 //We will devour a burger
-router.put("/api/burgers/eat/:id", function(req, res) {
+router.post("/api/burgers/eat/:id", function(req, res) {
     burger.updateOne(req.params.id, function(){
-        console.log("burger_controller.js put:" + req.params.id);
         res.redirect('/');
     });
 });
 
 
 //We will delete a burger
-//router.delete("/api/burgers/eat/:id", function(req, res) {
-  //  burger.deleteOne(req.params.id, function(){
-    //    res.redirect('/');
-   // });
-//});
-
-
-
+router.post("/api/burgers/delete/:id", function(req, res) {
+    burger.deleteOne(req.params.id, function(){
+        res.redirect('/');
+    });
+});
 
 
 //Export the routes
