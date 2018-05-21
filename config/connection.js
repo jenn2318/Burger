899c5ba0acc9b9a19dfbc6ebc2ff853ec0715a67
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-05-20 21:42:46 -0400

    commented out JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 let mysql = require("mysql");
 
+
 let connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
@@ -7,6 +8,20 @@ let connection = mysql.createConnection({
     password: "root",
     database: "burgers_db"
 });
+
+//if (process.env.JAWSDB_URL) {
+  //  connection = mysql.createConnection(process.env.JAWSDB_URL);
+//} //else {
+    //connection = mysql.createConnection({
+      //  host: "localhost",
+        //user: "root",
+        //password: "root",
+        //database: "burgers_db"
+    //});
+//};
+
+//connection.connect();
+
 
 // Make connection.
 connection.connect(function(err) {
